e533b8f7a500ac90f2aa3104ae0f4668a71a3fde
Colin <117777762+colinochieng@users.noreply.github.com>
2026-02-21 22:17:16 -0800

    Update project metadata in __about__.py
    Updated project metadata including title, URL, version, maintainer, and copyright.

--- src/quart_login/__about__.py
+++ src/quart_login/__about__.py
@@ -1,10 +1,10 @@
-__title__ = "Quart-Login"
+__title__ = "Quart-Login-Extended"
 __description__ = "User session management for Quart"
-__url__ = "https://github.com/0000matteo0000/quart-login"
-__version_info__ = ("0", "6", "1")
+__url__ = "https://github.com/colinochieng/quart-login"
+__version_info__ = ("0", "6", "2")
 __version__ = ".".join(__version_info__)
 __author__ = "Matteo Porta"
 __author_email__ = "leafstormrush@gmail.com"
-__maintainer__ = "Matteo Porta"
+__maintainer__ = "Colin Ochieng"
 __license__ = "MIT"
-__copyright__ = "(c) 2022 by Matteo Porta"
+__copyright__ = "(c) 2022 by Matteo Porta, (c) 2026 by Colin Ochieng"
